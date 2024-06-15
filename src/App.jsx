74dcd21af6277c924eabd01071fa6d34642a3ef9
Sayali Gayke <136413516+SAYALI8106@users.noreply.github.com>
2024-06-15 13:29:04 +0530

    add persoanl info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sayali Gayke",
+  title: "Full Stack MERN Developer",
+  email: "sayaligayke810@gmail.com",
+  gitHub: "https://github.com/SAYALI8106",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/sayali-gayke-9728592a7/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,7 +31,7 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Coding",
   "User experience",
   "Inclusive design",
   "Focus group testing",
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Passinated in Full satck";
 
 const About = () => {
   return (
